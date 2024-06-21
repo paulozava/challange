5a62d3836372d976e95bc80d207a23dee9b6efb4
paulozava <camposzava@gmail.com>
2024-06-21 12:00:52 +0100

    update databases host

--- app/databases.py
+++ app/databases.py
@@ -3,7 +3,7 @@ import psycopg
 
 def get_db_connection():
     conn = psycopg.connect(
-        "host=0.0.0.0 port=5432 dbname=hello user=root password=pantera!!!",
+        "host=postgres port=5432 dbname=hello user=root password=pantera!!!",
         autocommit=True,
     )
     return conn
