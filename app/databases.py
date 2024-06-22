f359db0c48f46709870c79845a1639715d8809a3
paulozava <camposzava@gmail.com>
2024-06-22 09:59:23 +0100

    reorg: easy the config to database

--- app/databases.py
+++ app/databases.py
@@ -2,10 +2,18 @@ import os
 
 import psycopg
 
+DEFAULT_DB = {
+    "host": "0.0.0.0",
+    "port": 5432,
+    "name": "hello",
+    "user": "root",
+    "password": "pantera!!!",
+}
+
 
 def get_db_connection():
     conn = psycopg.connect(
-        f"host={os.getenv('APP_DB_HOST', '0.0.0.0')} port={os.getenv('APP_DB_PORT', 5432)} dbname={os.getenv('APP_DB_NAME', 'hello')} user={os.getenv('APP_DB_USER', 'root')} password={os.getenv('APP_DB_PASSWORD', 'pantera!!!')}",
+        f"host={os.getenv('APP_DB_HOST', DEFAULT_DB['host'])} port={os.getenv('APP_DB_PORT', DEFAULT_DB['port'])} dbname={os.getenv('APP_DB_NAME', DEFAULT_DB['name'])} user={os.getenv('APP_DB_USER', DEFAULT_DB['user'])} password={os.getenv('APP_DB_PASSWORD', DEFAULT_DB['password'])}",
         autocommit=True,
     )
     return conn
