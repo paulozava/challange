7a3ea778e57967ffd6f66fc7fd6c33355484a425
paulozava <camposzava@gmail.com>
2024-06-22 21:20:47 +0100

    add nat gw to internal subnets

--- infrastructure/network.tf
+++ infrastructure/network.tf
@@ -68,35 +68,24 @@ resource "aws_subnet" "backend-private" {
   }
 }
 
-## Internet Gateway
-resource "aws_internet_gateway" "dmz-public" {
-  vpc_id = aws_vpc.main.id
+
+## NAT Gateway
+resource "aws_eip" "nat" {
+  for_each = aws_subnet.dmz-public
+  domain   = "vpc"
+}
+
+resource "aws_nat_gateway" "nat" {
+  for_each      = aws_subnet.dmz-public
+  allocation_id = aws_eip.nat[each.key].id
+  subnet_id     = each.value.id
 
   tags = {
-    Name = "${var.name}-igw"
+    Name = "${var.name}-nat-${each.key}"
   }
 }
 
-## Security Groups
 ## Route tables
-resource "aws_route_table" "dmz-public" {
-  vpc_id = aws_vpc.main.id
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.dmz-public.id
-  }
-
-  tags = {
-    Name = "${var.name}-dmz-public"
-  }
-}
-
-resource "aws_route_table_association" "dmz-public" {
-  for_each       = aws_subnet.dmz-public
-  subnet_id      = each.value.id
-  route_table_id = aws_route_table.dmz-public.id
-}
-
 resource "aws_route_table" "frontend-private" {
   for_each = aws_subnet.frontend-private
   vpc_id   = aws_vpc.main.id
@@ -119,4 +108,24 @@ resource "aws_route_table_association" "backend-private" {
   route_table_id = aws_route_table.backend-private[each.key].id
 }
 
+resource "aws_route" "frontend-nat" {
+  for_each               = aws_nat_gateway.nat
+  route_table_id         = aws_route_table.frontend-private[each.key].id
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id         = each.value.id
 
+  depends_on = [
+    aws_route_table.frontend-private
+  ]
+}
+
+resource "aws_route" "backend-nat" {
+  for_each               = aws_nat_gateway.nat
+  route_table_id         = aws_route_table.backend-private[each.key].id
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id         = each.value.id
+
+  depends_on = [
+    aws_route_table.backend-private
+  ]
+}
