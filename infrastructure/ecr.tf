aec62899886ad36a614717753afac83c5f87125e
paulozava <camposzava@gmail.com>
2024-06-24 08:12:31 +0100

    move to private reg

--- infrastructure/ecr.tf
+++ infrastructure/ecr.tf
@@ -1,8 +1,3 @@
-# resource "aws_ecrpublic_repository" "app" {
-#   provider        = aws.us_east_1
-#   repository_name = var.name
-# }
-#
 resource "aws_ecr_repository" "app" {
   name                 = var.name
   image_tag_mutability = "MUTABLE"
--- infrastructure/provider.tf
+++ infrastructure/provider.tf
@@ -19,9 +19,4 @@ provider "aws" {
   region = var.region
 }
 
-provider "aws" {
-  alias  = "us_east_1"
-  region = "us-east-1"
-}
-
 data "aws_region" "current" {}
