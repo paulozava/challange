d578d6b38d1f2c91b4380b06d4335e1258dfc189
paulozava <camposzava@gmail.com>
2024-06-24 17:32:37 +0100

    fix redeploy

--- start.tf
+++ start.tf
@@ -26,20 +26,28 @@ provider "aws" {
   region = var.region
 }
 
+locals {
+  docker_sha = sha1(timestamp())
+}
 
 module "infrastructure" {
-  source = "./infrastructure"
-  name   = var.name
-  region = var.region
+  source        = "./infrastructure"
+  name          = var.name
+  region        = var.region
+  image_version = local.docker_sha
 }
 
 resource "null_resource" "docker_build" {
   depends_on = [module.infrastructure]
+
+  triggers = {
+    infrastructure_image_sha = local.docker_sha
+  }
   provisioner "local-exec" {
     command = <<-EOT
         docker login -u AWS -p $(aws ecr get-login-password --region ${var.region}) ${module.infrastructure.image_reg}
-        docker build -t ${module.infrastructure.image_reg} .
-        docker push ${module.infrastructure.image_reg}
+        docker build -t ${module.infrastructure.image_reg}:${local.docker_sha} .
+        docker push ${module.infrastructure.image_reg}:${local.docker_sha}
     EOT
   }
 }
@@ -47,6 +55,8 @@ resource "null_resource" "docker_build" {
 output "app" {
   value = <<-EOT
 How to access your site:
+
+*Important: it could take few minutes to be available on the first deploy.
 
 To add an user:
 curl -X PUT -v -k https://${module.infrastructure.access_your_site}/hello/<username> -H "Content-Type: application/json" -d '{"dateOfBirth": "1990-01-01"}'
